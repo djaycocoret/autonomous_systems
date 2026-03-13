e6c576a06e977e5c69146b381edcffa6969e39ec
djayco <d.coret@icloud.com>
2026-03-13 13:36:20 +0100

    two wheel drive

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from gpiozero import DigitalOutputDevice
 # Define pins to match your Arduino variables
 forward_pin = DigitalOutputDevice(17)
 backward_pin = DigitalOutputDevice(27)
+forward_pin_2 = DigitalOutputDevice(23)
+backward_pin_2 = DigitalOutputDevice(24)
 delay_time = 2.0  # seconds
 
 try:
@@ -12,24 +14,32 @@ try:
         # Stop
         forward_pin.off()
         backward_pin.off()
+        forward_pin_2.off()
+        backward_pin_2.off()
         print("Stop")
         sleep(delay_time)
 
         # Spin Forward
         forward_pin.on()
         backward_pin.off()
+        forward_pin_2.on()
+        backward_pin_2.off()
         print("Forward")
         sleep(delay_time)
 
         # Stop
         forward_pin.off()
         backward_pin.off()
+        forward_pin_2.off()
+        backward_pin_2.off()
         print("Stop")
         sleep(delay_time)
 
         # Spin Backward
         forward_pin.off()
         backward_pin.on()
+        forward_pin_2.off()
+        backward_pin_2.on()
         print("Backward")
         sleep(delay_time)
 
@@ -37,3 +47,5 @@ except KeyboardInterrupt:
     # Clean exit if you press Ctrl+C
     forward_pin.off()
     backward_pin.off()
+    forward_pin_2.off()
+    backward_pin_2.off()
